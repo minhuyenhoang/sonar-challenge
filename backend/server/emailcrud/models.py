daf45ab0aa17b2af110f0568ebca730bac632a42
Uyen Hoang <minhuyen0512@gmail.com>
2022-08-18 12:18:17 -0700

    Create CRUD actions

--- backend/server/emailcrud/models.py
+++ backend/server/emailcrud/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.utils import timezone
 
 # Create your models here.
+class User(models.Model):
+  first_name = models.CharField(max_length=10, default='')
+  last_name = models.CharField(max_length=10, default='')
+  email = models.CharField(max_length=100, default='')
+  frequency = ArrayField(models.IntegerField(), size=3, blank=True, default=list)
+  created_at = models.DateTimeField(default=timezone.now)
+  updated_at = models.DateTimeField(default=timezone.now)
---
+++ backend/server/emailcrud/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from emailcrud.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+  email = serializers.EmailField(required=True)
+  class Meta: 
+    model = User
+    fields = ['id', 'first_name', 'last_name', 'email', 'frequency']
